67ecf1915f2ea924fb3f3b9ed8606a36df869b9f
KhodakovskaMarya <khodakovska.marya@gmail.com>
2022-07-11 09:41:40 +0300

    Add HTML and php

---
+++ starter.php
@@ -0,0 +1,14 @@
+<?php
+$num = rand(1,4);
+echo $num;
+echo '<dir>';
+if($num==1){
+	echo "winter";
+}elseif ($num==2){
+	echo "spring";
+}elseif ($num==3){	
+	echo "summer";
+}else{	
+	echo "autem"; 
+}
+
